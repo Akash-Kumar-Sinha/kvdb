86ecb0ae19422a7782762cb9e641221cc30c7c8d
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2026-08-04 02:48:38 +0530

    minor fix

--- btree/src/lib.rs
+++ btree/src/lib.rs
@@ -1,9 +1,9 @@
 mod btree;
+mod error;
 mod pager;
 mod value;
-mod error;
 
 pub use btree::{BTree, Initialized, Locked, Node, Uninitialized, Unlocked};
+pub use error::ValueError;
 pub use pager::{PageId, Pager};
 pub use value::Value;
-pub use error::ValueError;
